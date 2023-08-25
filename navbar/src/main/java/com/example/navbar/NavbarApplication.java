dd728c7d666e54fbe17130c0effb37c413e2ad21
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2023-08-24 21:12:17 -0500

    creacion de proyecto

---
+++ navbar/src/main/java/com/example/navbar/NavbarApplication.java
@@ -0,0 +1,13 @@
+package com.example.navbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavbarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavbarApplication.class, args);
+    }
+
+}
